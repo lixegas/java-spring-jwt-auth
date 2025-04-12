fdd1fb5c9b7220436086665f7e3ce55259d5634d
Lixegas <167356473+lixegas@users.noreply.github.com>
2025-04-12 16:26:44 +0200

    first commit

---
+++ src/test/java/com/lixegas/jwt_auth/JwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lixegas.jwt_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
